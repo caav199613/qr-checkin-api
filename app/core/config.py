120446ee351ff9d77e842261e7f87ea65a512662
Cristian Arias <ca.ariasv1@uniandes.edu.co>
2025-08-11 21:29:22 -0500

    Fix Config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv() 
 
-DB_NAME = os.getenv("DB_NAME", "admin")
+DB_NAME = os.getenv("DB_NAME", "usuariosdb")
 DB_USER = os.getenv("DB_USER", "admin")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "usuariosdb")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "admin")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "3306")
 
